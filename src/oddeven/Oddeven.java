600a47a977048ed9e775ada7c76ebd7c160f579d
Guntur Wicaksono <gunturw79@gmail.com>
2020-08-15 16:30:45 +0700

    oddeven

---
+++ src/oddeven/Oddeven.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oddeven;
+
+/**
+ *
+ * @author 247
+ */
+import java.util.Scanner;
+public class Oddeven {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int number = reader.nextInt();
+        if (number % 2 == 0){
+            System.out.printf("Angka %d adalah Genap", number);
+        }else{
+            System.out.printf("Angka %d adalah Ganjil", number);
+        }
+    }
+    
+}
